8d597a150a4682a112bcf45a619eda9b4a14428f
M-coder-png <maletshankepana@gmail.com>
2025-11-06 16:37:18 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ function displayTemperature(response) {
   getForecast(response.data.city);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
@@ -80,23 +88,29 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
   let forecastHtml = "";
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
 <div class="weather-forecast-day">
-  <div class="weather-forecast-date">${day}</div>
-  <div class="weather-forecast-icon">🌤️</div>
+  <div class="weather-forecast-date">${formatDay(day.time)}</div>
+  
+  <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+  
   <div class="weather-forecast-temperatures">
   <div class="weather-forecast-temperature"> 
-  <strong>15°</strong>
+  <strong>${Math.round(day.temperature.maximum)}°</strong>
   </div>
-    <div class="weather-forecast-temperature">9°</div>
+  <div class="weather-forecast-temperature"> 
+  ${Math.round(day.temperature.minimum)}°
+  </div>
+    
   </div>
 </div>
 `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
